cc8050309186198dc0379fe708b7f321ecb16dc3
Blake Strickland <Stricklandb02@gmail.com>
2016-02-09 23:33:04 -0500

    Test 21 passing

--- grid.rb
+++ grid.rb
@@ -27,7 +27,6 @@ class Grid
     return false if has_ship_on?(x, y)
   end
 
-
   def display
     letters = ["A","B","C","D","E","F","G","H","I","J"]
     puts "    1   2   3   4   5   6   7   8   9   10"
